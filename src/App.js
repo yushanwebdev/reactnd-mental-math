fd829f614ac94df231add59f2819d53b7248b2c0
yushan <yushanwebdev@gmail.com>
2020-12-29 22:54:19 +0530

    feat: Develop the Mental Math Game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Game from './Game';
+import Score from './Score';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    correctAnswer: 0,
+    numQuestions: 0
+  }
+
+  handleAnswer = answerWasCorrect => {
+    if(answerWasCorrect) {
+      this.setState(currState => ({
+        correctAnswer: currState.correctAnswer + 1
+      }));
+    }
+    this.setState(currState => ({
+      numQuestions: currState.numQuestions + 1
+    }))
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <div className="game">
+          <h2>Mental Math</h2>
+          <Game handleAnswer={this.handleAnswer} />
+          <Score numCorrect={this.state.correctAnswer} numQuestions={this.state.numQuestions} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
